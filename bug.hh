82d19427fa094883d0d73093e2df87c92416458d
BugHunter-X <askzenai@gmail.com>
2025-01-24 10:52:05 +0100

    Add bug.hh file

---
+++ bug.hh
@@ -0,0 +1,20 @@
+function foo(x: int): int {
+  if (x == 0) {
+    return 1;
+  } else {
+    return x * foo(x - 1);
+  }
+}
+
+function bar(x: int): int {
+  if (x < 0) {
+    return 0;
+  } else {
+    return foo(x);
+  }
+}
+
+<<main>>
+function main(): void {
+  echo bar(5);
+}
